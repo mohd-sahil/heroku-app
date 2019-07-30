a15ba00f272bf6f7ec46a79f31de11050a10b896
unknown <beingsahil99@gmail.com>
2019-07-30 19:56:19 +0530

    removed js

--- src/components/News.js
+++ src/components/News.js
@@ -3,23 +3,61 @@ import React, { Component } from "react";
 class News extends Component {
   render() {
     return (
-      <section className="banner-bottom" id="news">
-        <div className="banner-top row middle-grids">
-          <div className="col-lg-4 advantage-grid-info1">
-            <div className="advantage_left2 text-center" />
-          </div>
-          <div className="col-lg-8 advantage-grid-info">
-            <div className="advantage_left">
-              <h3>Integer porttitor mollisar lorem, at molestie</h3>
-              <p className="mt-4">
-                Nulla pellentesque mi non laoreet eleifend. Integer porttitor
-                mollisar lorem, at molestie arcu pulvinar ut. Proin ac fermentum
-                est.
-              </p>
+      <div>
+        <section className="banner-bottom" id="news">
+          <div className="banner-top row middle-grids">
+            <div className="col-lg-4 advantage-grid-info1">
+              <div className="advantage_left2 text-center" />
+            </div>
+            <div className="col-lg-8 advantage-grid-info">
+              <div className="advantage_left">
+                <h3>Integer porttitor mollisar lorem, at molestie</h3>
+                <p className="mt-4">
+                  Nulla pellentesque mi non laoreet eleifend. Integer porttitor
+                  mollisar lorem, at molestie arcu pulvinar ut. Proin ac
+                  fermentum est.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+
+        <section className="banner-bottom temp-btm">
+          <div className="banner-top row middle-grids">
+            <div className="col-lg-8 advantage-grid-info">
+              <div className="advantage_left">
+                <h3>Integer porttitor mollisar loremr, at molestie</h3>
+                <p className="mt-4">
+                  Nulla pellentesque mi non laoreet eleifend. Integer porttitor
+                  mollisar lorem, at molestie arcu pulvinar ut. Proin ac
+                  fermentum est.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4 advantage-grid-info1 second">
+              <div className="advantage_left2 text-center" />
+            </div>
+          </div>
+        </section>
+
+        <section className="banner-bottom">
+          <div className="banner-top row middle-grids">
+            <div className="col-lg-4 advantage-grid-info1 third">
+              <div className="advantage_left2 text-center" />
+            </div>
+            <div className="col-lg-8 advantage-grid-info">
+              <div className="advantage_left">
+                <h3>Integer porttitor mollisar lorem, at molestie</h3>
+                <p className="mt-4">
+                  Nulla pellentesque mi non laoreet eleifend. Integer porttitor
+                  mollisar lorem, at molestie arcu pulvinar ut. Proin ac
+                  fermentum est.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -15,38 +15,8 @@ class App extends Component {
 
        <About />
        
-      <News /> 
-       <section className="banner-bottom temp-btm">
-           <div className="banner-top row middle-grids">
-               <div className="col-lg-8 advantage-grid-info">
-                   <div className="advantage_left">
-                       <h3>Integer porttitor mollisar loremr, at molestie</h3>
-                       <p className="mt-4">Nulla pellentesque mi non laoreet eleifend. Integer porttitor mollisar lorem, at molestie arcu pulvinar ut. Proin ac fermentum est.</p>
-                   </div>
-               </div>
-               <div className="col-lg-4 advantage-grid-info1 second">
-                   <div className="advantage_left2 text-center">
-     
-                   </div>
-               </div>
-           </div>
-       </section>
+       <News/>
       
-       <section className="banner-bottom">
-           <div className="banner-top row middle-grids">
-               <div className="col-lg-4 advantage-grid-info1 third">
-                   <div className="advantage_left2 text-center">
-     
-                   </div>
-               </div>
-               <div className="col-lg-8 advantage-grid-info">
-                   <div className="advantage_left">
-                       <h3>Integer porttitor mollisar lorem, at molestie</h3>
-                       <p className="mt-4">Nulla pellentesque mi non laoreet eleifend. Integer porttitor mollisar lorem, at molestie arcu pulvinar ut. Proin ac fermentum est.</p>
-                   </div>
-               </div>
-           </div>
-       </section>
        <section className="slide-wrapper py-5" id="service">
            <div className="container py-lg-5">
                <div className="text-center">
